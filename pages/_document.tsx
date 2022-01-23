e9029f9967ea9d015f6c0e2d12a15eb5b4042b93
nokkup <kuprin.kirill.96@gmail.com>
2022-01-23 21:30:56 +0300

    fix: add link element inside the head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,9 @@ import Document, {
 } from "next/document";
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    static async getInitialProps(
+        ctx: DocumentContext
+    ): Promise<DocumentInitialProps> {
         const initialProps = await Document.getInitialProps(ctx);
         return { ...initialProps };
     }
@@ -16,12 +18,13 @@ class MyDocument extends Document {
     render(): JSX.Element {
         return (
             <Html lang="en">
-                <Head />
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500&display=swap"
-                    rel="stylesheet"
-                />
+                <Head>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
